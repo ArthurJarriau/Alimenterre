8c81ea63789a05c10217faba1b4157ed96e46b18
Dijoux Nicolas <nicolas.dijoux@etudiant.univ-lr.fr>
2024-01-25 13:48:38 +0100

    homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Bienvenue sur AlimenTerre</h1>
+        <p>Notre projet vise à vous aider à faire des choix alimentaires plus sains et plus respectueux de l'environnement.</p>
+        <p>Vous pouvez rechercher des produits par nom ou par code barre.</p>
       </header>
+      <section>
+        <h2>EcoScore</h2>
+        <p>L'EcoScore est un indicateur environnemental qui vous aide à comprendre l'impact de vos choix alimentaires sur l'environnement. Il prend en compte des facteurs tels que les émissions de gaz à effet de serre, l'utilisation des terres et l'épuisement des ressources naturelles.</p>
+        
+      </section>
+      <section>
+        <h2>NutriScore</h2>
+        <p>Le NutriScore est un système de notation nutritionnelle qui vous aide à comprendre la qualité nutritionnelle des aliments. Il est basé sur une échelle de couleurs et de lettres allant de A (meilleur choix) à E (moins bon choix).</p>
+      </section>
     </div>
   );
 }
