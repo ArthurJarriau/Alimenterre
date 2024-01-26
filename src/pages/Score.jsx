658c1438953158e73d39fd24640d75156bcc7385
ptallon <paul.tallon@etudiant.univ-lr.fr>
2024-01-26 11:07:33 +0100

    leaderboard + bd

--- src/pages/Score.jsx
+++ src/pages/Score.jsx
@@ -1,74 +1,75 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthContext } from "../components/App";
 import CardHisto from "../components/CardHisto";
 import LeaderBoardCard from "../components/LeaderBoardCard";
 import ScoreCard from "../components/ScoreCard";
 import ScoreTracker from "../components/ScoreTracker";
-const Home = () => {
-  const { loginState } = useContext(AuthContext);
+const Score = () => {
+  const { loginState, handleLogout } = useContext(AuthContext);
+  const [users,setUsers]= useState()
+  const getUsers = () => {
+    fetch("/api/users", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const sortedUsers = data.sort((a, b) => b.points_gen - a.points_gen);
+        setUsers(sortedUsers);
+      });
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+  useEffect(() => {
+    getUsers()
+  }, []);
   if (loginState.user === null) {
     return <Navigate to="/login" />;
   }
+
   return (
     <section className="score">
       <div className="score-global">
-        <ScoreTracker score={75} size={"large"} />
+        <ScoreTracker score={loginState.user.points_gen} size={"large"} />
         <h2 className="score-global__title">Score</h2>
       </div>
       <div className="card-list">
-        <h2 className="card-list__title">Détails des scores</h2>
-        <ScoreCard
-          score={75}
-          title={"Score de transport"}
-          svg={"truck"}
-          color={"#F47D75"}
-        />
-        <ScoreCard
-          score={50}
-          title={"Eco score"}
-          svg={"ecoscore"}
-          color={"#BDDCFC"}
-        />
-        <ScoreCard
-          score={25}
-          title={"Epi score"}
-          svg={"episcore"}
-          color={"#D2F475"}
-        />
-        <ScoreCard
-          score={15}
-          title={"Nutriscore"}
-          svg={"nutriscore"}
-          color={"#F47D75"}
-        />
-      </div>
+        <h2 className="card-list__title">Leaderboard</h2>
+      
       <LeaderBoardCard
         position="1st"
-        name="Jean-Marie DURAND"
-        points="206710"
+        name={users && users[0].username}
+        points={users && users[0].points_gen}
         bgColor="#F47D75"
       />
       <LeaderBoardCard
         position="2nd"
-        name="KeepSand"
-        points="206450"
+        name={users && users[1].username}
+        points={users && users[1].points_gen}
         bgColor="#BDDCFC"
       />
       <LeaderBoardCard
         position="3rd"
-        name="OkayOkay33000"
-        points="206400"
+        name={users && users[2].username}
+        points={users && users[2].points_gen}
         bgColor="#D2F475"
       />
+
+      {users && (loginState.user.points_gen<users[2].points_gen  &&
       <LeaderBoardCard
-        position="49837th"
-        name="Philippe KAMO"
-        points="2710"
+        position={(users.findIndex(user => user.email === loginState.user.email))}
+        name={loginState.user.username}
+        points={loginState.user.points_gen}
         bgColor="#F47D75"
-      />
+      />)}
+      </div>
     </section>
   );
 };
 
-export default Home;
+export default Score;
